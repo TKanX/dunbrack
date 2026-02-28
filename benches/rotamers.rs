32c6affd171c83155550d682c2b9228cf83e08bc
Tony Kan <tianchengkan@gmail.com>
2026-02-28 05:07:12 -0800

    test(benches): Centralize for_all_residues macro

--- benches/rotamers.rs
+++ benches/rotamers.rs
@@ -1,5 +1,6 @@
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use dunbrack::Residue;
+use dunbrack::for_all_residues;
 use dunbrack::{
     Arg, Asn, Asp, Cpr, Cyd, Cyh, Cys, Gln, Glu, His, Ile, Leu, Lys, Met, Phe, Pro, Ser, Thr, Tpr,
     Trp, Tyr, Val,
@@ -11,33 +12,6 @@ const PSI: f32 = -43.0;
 const GRID_COUNT: usize = 37;
 const GRID_MIN: f32 = -180.0;
 const GRID_STEP: f32 = 10.0;
-
-macro_rules! for_all_residues {
-    ($callback:ident) => {
-        $callback!(Arg, 4, 75);
-        $callback!(Asn, 2, 36);
-        $callback!(Asp, 2, 18);
-        $callback!(Cpr, 3, 2);
-        $callback!(Cyd, 1, 3);
-        $callback!(Cyh, 1, 3);
-        $callback!(Cys, 1, 3);
-        $callback!(Gln, 3, 108);
-        $callback!(Glu, 3, 54);
-        $callback!(His, 2, 36);
-        $callback!(Ile, 2, 9);
-        $callback!(Leu, 2, 9);
-        $callback!(Lys, 4, 73);
-        $callback!(Met, 3, 27);
-        $callback!(Phe, 2, 18);
-        $callback!(Pro, 3, 2);
-        $callback!(Ser, 1, 3);
-        $callback!(Thr, 1, 3);
-        $callback!(Tpr, 3, 2);
-        $callback!(Trp, 2, 36);
-        $callback!(Tyr, 2, 18);
-        $callback!(Val, 1, 3);
-    };
-}
 
 fn bench_query(c: &mut Criterion) {
     let mut group = c.benchmark_group("query");
